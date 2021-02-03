5f15dc19a840af0f3b1ac52db5ecd49df2403e80
Andy Strube <andystrube64@gmail.com>
2021-02-02 16:44:42 -0800

    testing 123

--- db/schema.rb
+++ db/schema.rb
@@ -15,10 +15,6 @@ ActiveRecord::Schema.define(version: 2020_08_17_231434) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "view_counters", force: :cascade do |t|
-    t.integer "views"
-  end
-
   create_table "visits", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
